411abeac6573d106879216f746a3a6604d996272
7monet <60078817+7monet@users.noreply.github.com>
2021-04-14 14:53:21 +0200

    Add files via upload

--- controllers.js
+++ controllers.js
@@ -1,25 +1,23 @@
 var portfolioApp = angular.module('portfolioApp',[]);
 
-portfolioApp.controller('GalleryListCtrl', function($scope)
-{
-    $scope.galleries = 
-    [
-        { 'title':'Drukarka 3D ',
-        'when': new Date('12-09-2012'),
-        'thumbnailUrl':'img/g1.jpg'
-        },
-        { 'title':'przecinarka plazmowa CNC',
-        'when': new Date('01-11-2014'),
-        'thumbnailUrl':'img/g2.jpg'
-        },
-        { 'title':'Drukarka 3D SLA',
-        'when' : new Date('04-29-2016'),
-        'thumbnailUrl':'img/g3.jpg'
-        },
-        { 'title':'Frezarka CNC',
-        'when' : new Date('09-21-2020'),
-        'thumbnailUrl':'img/g4.jpg'
-        }
-    ];
-    $scope.galleries.length;
+
+
+portfolioApp.controller('GalleryListCtrl', ['$scope', '$http', function($scope, $http) {
+$http.get('img/galleries.json').success(function(data) {
+$scope.galleries=data; 
 });
+//lab7
+$scope.galleries.length;
+$scope.sortList = [
+    {
+        'label':'Alfabetycznie',
+        'value':'title'
+    },
+    {
+        'label':'Chronologicznie',
+        'value':'when'
+    }
+];
+$scope.orderProp = 'when';
+
+}]);
